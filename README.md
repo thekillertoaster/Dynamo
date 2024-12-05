c82b4685dbb928865a2483270bf8957f12b4d2cf
Aaron Hands <thekillertoaster@gmail.com>
2024-12-05 18:52:12 +0000

    pretty formatting

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ An Electron application boilerplate with React and TypeScript ( & Shadcn )
 
 ![Screenshot (9)](https://github.com/p32929/electron-shadcn-typescript/assets/6418354/c07ff9dd-6434-45aa-9620-c9536c8db168)
 
-
 ## Recommended IDE Setup
 
 - [VSCode](https://code.visualstudio.com/) + [ESLint](https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint) + [Prettier](https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode)
--- src/renderer/src/components/ui/input-otp.tsx
+++ src/renderer/src/components/ui/input-otp.tsx
@@ -1,41 +1,37 @@
-"use client"
+'use client'
 
-import * as React from "react"
-import { DashIcon } from "@radix-ui/react-icons"
-import { OTPInput, SlotProps } from "input-otp"
+import * as React from 'react'
+import { DashIcon } from '@radix-ui/react-icons'
+import { OTPInput, SlotProps } from 'input-otp'
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils'
 
 const InputOTP = React.forwardRef<
   React.ElementRef<typeof OTPInput>,
   React.ComponentPropsWithoutRef<typeof OTPInput>
 >(({ className, ...props }, ref) => (
-  <OTPInput
-    ref={ref}
-    containerClassName={cn("flex items-center gap-2", className)}
-    {...props}
-  />
+  <OTPInput ref={ref} containerClassName={cn('flex items-center gap-2', className)} {...props} />
 ))
-InputOTP.displayName = "InputOTP"
+InputOTP.displayName = 'InputOTP'
 
 const InputOTPGroup = React.forwardRef<
-  React.ElementRef<"div">,
-  React.ComponentPropsWithoutRef<"div">
+  React.ElementRef<'div'>,
+  React.ComponentPropsWithoutRef<'div'>
 >(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("flex items-center", className)} {...props} />
+  <div ref={ref} className={cn('flex items-center', className)} {...props} />
 ))
-InputOTPGroup.displayName = "InputOTPGroup"
+InputOTPGroup.displayName = 'InputOTPGroup'
 
 const InputOTPSlot = React.forwardRef<
-  React.ElementRef<"div">,
-  SlotProps & React.ComponentPropsWithoutRef<"div">
+  React.ElementRef<'div'>,
+  SlotProps & React.ComponentPropsWithoutRef<'div'>
 >(({ char, hasFakeCaret, isActive, className, ...props }, ref) => {
   return (
     <div
       ref={ref}
       className={cn(
-        "relative flex h-9 w-9 items-center justify-center border-y border-r border-input text-sm shadow-sm transition-all first:rounded-l-md first:border-l last:rounded-r-md",
-        isActive && "z-10 ring-1 ring-ring",
+        'relative flex h-9 w-9 items-center justify-center border-y border-r border-input text-sm shadow-sm transition-all first:rounded-l-md first:border-l last:rounded-r-md',
+        isActive && 'z-10 ring-1 ring-ring',
         className
       )}
       {...props}
@@ -49,16 +45,16 @@ const InputOTPSlot = React.forwardRef<
     </div>
   )
 })
-InputOTPSlot.displayName = "InputOTPSlot"
+InputOTPSlot.displayName = 'InputOTPSlot'
 
 const InputOTPSeparator = React.forwardRef<
-  React.ElementRef<"div">,
-  React.ComponentPropsWithoutRef<"div">
+  React.ElementRef<'div'>,
+  React.ComponentPropsWithoutRef<'div'>
 >(({ ...props }, ref) => (
   <div ref={ref} role="separator" {...props}>
     <DashIcon />
   </div>
 ))
-InputOTPSeparator.displayName = "InputOTPSeparator"
+InputOTPSeparator.displayName = 'InputOTPSeparator'
 
 export { InputOTP, InputOTPGroup, InputOTPSlot, InputOTPSeparator }
