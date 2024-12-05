2cb537599bd55097df9e8be925a72188a66fda4b
Aaron Hands <thekillertoaster@gmail.com>
2024-12-05 18:46:18 +0000

    - Converted line endings - Added missing return types to utility functions - Converted double quotes to singles for prettifier - Reordered imports

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,23 +1,16 @@
-
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { useState } from "react"
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { useState } from 'react'
 
 function App(): JSX.Element {
   // const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
   const [counter, setCounter] = useState(0)
 
-  const increase = () => {
+  const increase = (): void => {
     setCounter(counter + 1)
   }
 
-  const decrease = () => {
+  const decrease = (): void => {
     setCounter(counter - 1)
   }
 
@@ -28,12 +21,12 @@ function App(): JSX.Element {
           <CardTitle>Counter: {counter}</CardTitle>
         </CardHeader>
         <CardContent>
-          <p>
-            Click on increase / decrease to check the state changes
-          </p>
+          <p>Click on increase / decrease to check the state changes</p>
         </CardContent>
         <CardFooter className="flex justify-between">
-          <Button onClick={decrease} variant="outline">Decrease --</Button>
+          <Button onClick={decrease} variant="outline">
+            Decrease --
+          </Button>
           <Button onClick={increase}>Increase ++</Button>
         </CardFooter>
       </Card>
--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import "./global.css";
-import App from './App';
-import { ThemeProvider } from './components/theme-provider';
+import './global.css'
+import App from './App'
+import { ThemeProvider } from './components/theme-provider'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
